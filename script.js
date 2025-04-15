53e1470f4857714eaf881f16ec845a0dc17e9e81
Srireddy88 <lakshmisri0805@gmail.com>
2025-04-15 18:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
   const name = document.getElementById('name').value;
-  document.getElementById('confirmation').textContent = `Thank you, ${name}! I will get back to you soon.`;
+  document.getElementById('confirmation').textContent = `Thank you,I will get back to you soon.`;
   this.reset();
 });
